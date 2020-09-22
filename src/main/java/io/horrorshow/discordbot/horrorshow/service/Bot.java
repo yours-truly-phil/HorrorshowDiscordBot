a4eeb50b3bb3ca17daacd70194d69b9a40a475c4
Philipp Seeger <philippbseeger@googlemail.com>
2020-09-22 14:44:55 +0200

    store / remove / print messages with pizza reaction

--- src/main/java/io/horrorshow/discordbot/horrorshow/service/Bot.java
+++ src/main/java/io/horrorshow/discordbot/horrorshow/service/Bot.java
@@ -3,8 +3,11 @@ package io.horrorshow.discordbot.horrorshow.service;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 import javax.security.auth.login.LoginException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -22,6 +28,8 @@ public class Bot extends ListenerAdapter {
 
     private static final String EMOJI_PIZZA = "\uD83C\uDF55";
 
+    private static final String CMD_PRINT_MESSAGES = "\\$messages";
+    private static final Map<String, Message> messages = new HashMap<>();
     private static JDA jda;
 
     public Bot(@Autowired @Value("${" + PROP_TOKEN + "}") String TOKEN) {
@@ -39,7 +47,21 @@ public class Bot extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if (!event.getAuthor().isBot()) {
+            var rawMsgContent = event.getMessage().getContentRaw();
+            if (rawMsgContent.matches(CMD_PRINT_MESSAGES)) {
+                printMessages(event.getChannel());
+            }
+        }
+    }
 
+    private void printMessages(MessageChannel channel) {
+        var message = messages.entrySet().stream()
+                .map(entry -> "Msg " + entry.getKey() +
+                        " by " + entry.getValue().getAuthor() +
+                        "\n" + entry.getValue().getContentRaw())
+                .collect(Collectors.joining("\n"));
+        channel.sendMessage((!message.isEmpty()) ? message : "no messages stored").queue();
     }
 
     @Override
@@ -47,6 +69,30 @@ public class Bot extends ListenerAdapter {
         var emoji = event.getReaction().getReactionEmote().getEmoji();
         if (EMOJI_PIZZA.equals(emoji)) {
             event.getChannel().sendMessage(EMOJI_PIZZA + " reaction found!").queue();
+            var messageId = event.getMessageId();
+            event.getChannel()
+                    .retrieveMessageById(messageId)
+                    .queue(message -> {
+                        messages.put(message.getId(), message);
+                        log.info("Added message {}", message);
+                    });
+        }
+    }
+
+    @Override
+    public void onMessageReactionRemove(@NotNull MessageReactionRemoveEvent event) {
+        var emoji = event.getReaction().getReactionEmote().getEmoji();
+        if (EMOJI_PIZZA.equals(emoji)) {
+            var messageId = event.getMessageId();
+            event.getChannel()
+                    .retrieveMessageById(messageId)
+                    .queue(message -> {
+                        if (message.getReactions().stream()
+                                .noneMatch(reaction -> reaction.getReactionEmote().getEmoji().equals(EMOJI_PIZZA))) {
+                            log.info("Removed message due to no more pizza {}", message);
+                            messages.remove(message.getId());
+                        }
+                    });
         }
     }
 }
