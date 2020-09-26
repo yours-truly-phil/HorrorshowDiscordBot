aa4e2add4626cc72f7e06ccd8f39e7da22ba5bf0
Philipp Seeger <philippbseeger@googlemail.com>
2020-09-26 23:11:49 +0200

    async / multi thread on message received

--- src/main/java/io/horrorshow/discordbot/horrorshow/HorrorshowDiscordBotApplication.java
+++ src/main/java/io/horrorshow/discordbot/horrorshow/HorrorshowDiscordBotApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync(proxyTargetClass = true)
 public class HorrorshowDiscordBotApplication {
 
     private static ConfigurableApplicationContext context;
--- src/main/java/io/horrorshow/discordbot/horrorshow/configuration/BinanceApiConfiguration.java
+++ src/main/java/io/horrorshow/discordbot/horrorshow/configuration/BinanceApiConfiguration.java
@@ -4,6 +4,9 @@ import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 public class BinanceApiConfiguration {
@@ -11,6 +14,17 @@ public class BinanceApiConfiguration {
     @Bean
     public BinanceApiRestClient binanceApiRestClient() {
         return BinanceApiClientFactory.newInstance().newRestClient();
+    }
+
+    @Bean
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("Thread-");
+        executor.initialize();
+        return executor;
     }
 
 }
